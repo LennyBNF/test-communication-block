8e90d3574b43a3cde04d6b4f69adf6faeef5e9e3
LennyBNF <132681048+LennyBNF@users.noreply.github.com>
2023-05-15 09:02:10 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("A")
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # # # # #
+    # # . # #
+    # . . . #
+    # # . # #
+    # # # # #
+    `)
+basic.pause(500)
+basic.showString("!")
+basic.forever(function () {
+    serial.writeLine("Intensité lumineuse")
+    serial.writeNumber(input.lightLevel())
+    serial.writeLine("")
+    serial.writeLine("Temperature")
+    serial.writeNumber(input.temperature())
+    serial.writeLine("")
+    serial.writeLine("Champs magnetique")
+    serial.writeNumber(input.magneticForce(Dimension.X))
+    serial.writeLine("")
+    basic.pause(1000)
 })
-input.onButtonPressed(Button.AB, function () {
-    radio.sendString("")
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("B")
-})
-radio.setGroup(9)
